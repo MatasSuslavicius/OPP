fa2c514903cf7870fbf8df2724b79d40f04f826b
Antanas Babič <antbab@ktu.lt>
2022-09-24 13:56:08 +0300

    Add background image

--- tower-battle/tower-battle/ClientApp/src/App.tsx
+++ tower-battle/tower-battle/ClientApp/src/App.tsx
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.scss';
 
 function App(): JSX.Element {
+  const myStyle = {
+    backgroundImage: 'url(/background.JPG)',
+    height: '80vh',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={myStyle}>
+      zzz
     </div>
-  );
+  )
 }
 
 export default App;
