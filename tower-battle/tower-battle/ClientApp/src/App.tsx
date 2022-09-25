1ef8f691b4ebcbedbd4da23ce1d85a9784a3a5f7
Antanas Babič <antbab@ktu.lt>
2022-09-25 13:30:49 +0300

    Add interface

--- tower-battle/tower-battle/ClientApp/src/App.tsx
+++ tower-battle/tower-battle/ClientApp/src/App.tsx
@@ -1,17 +1,9 @@
 import './App.css';
+import { Interface } from './components/interface/Interface';
 
 function App(): JSX.Element {
-  const myStyle = {
-    backgroundImage: 'url(/background.JPG)',
-    height: '80vh',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat'
-  }
-
   return (
-    <div style={myStyle}>
-      zzz
-    </div>
+    <Interface></Interface>
   )
 }
 
---
+++ tower-battle/tower-battle/ClientApp/src/components/interface/Interface.tsx
@@ -0,0 +1,8 @@
+import './Interface.css'
+export function Interface(): JSX.Element {
+  return (
+    <div className='Background'>
+      
+    </div>
+  )
+}
