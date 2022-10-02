24776d5b27f85dea094442b64696cd34c6c594ba
Dominykas Plesevičius <dominykasplesevicius@gmail.com>
2022-10-02 13:55:34 +0300

    Add collisions

--- backend/Services/GameLogic.cs
+++ backend/Services/GameLogic.cs
@@ -13,13 +13,55 @@ namespace tower_battle.Services
         {
             foreach(var leftPlayerUnit in state.LeftPlayerState.Units)
             {
-                leftPlayerUnit.Position.X += leftPlayerUnit.Speed * GameManager.UPDATE_TIME;
+                if(!IsUnitColliding(leftPlayerUnit, state))
+                {
+                    leftPlayerUnit.Position.X += leftPlayerUnit.Speed * GameManager.UPDATE_TIME;
+                }
             }
 
             foreach (var rightPlayerUnit in state.RightPlayerState.Units)
             {
-                rightPlayerUnit.Position.X -= rightPlayerUnit.Speed * GameManager.UPDATE_TIME;
+                if (!IsUnitColliding(rightPlayerUnit, state))
+                {
+                    rightPlayerUnit.Position.X -= rightPlayerUnit.Speed * GameManager.UPDATE_TIME;
+                }
             }
+        } 
+
+        private static bool IsUnitColliding(Unit unit, GameStateSingleton state)
+        {
+            foreach(Unit leftPlayerUnit in state.LeftPlayerState.Units)
+            {
+                if(unit != leftPlayerUnit && 
+                    PositionRangesOverlap(
+                        unit.Position.X - (unit.Scale.X / 2),
+                        unit.Position.X + (unit.Scale.X / 2),
+                        leftPlayerUnit.Position.X - (leftPlayerUnit.Scale.X / 2),
+                        leftPlayerUnit.Position.X + (leftPlayerUnit.Scale.X / 2)))
+                {
+                    return true;
+                }
+            }
+
+            foreach (Unit rightPlayerUnit in state.RightPlayerState.Units)
+            {
+                if (unit != rightPlayerUnit &&
+                    PositionRangesOverlap(
+                        unit.Position.X - (unit.Scale.X / 2),
+                        unit.Position.X + (unit.Scale.X / 2),
+                        rightPlayerUnit.Position.X - (rightPlayerUnit.Scale.X / 2),
+                        rightPlayerUnit.Position.X + (rightPlayerUnit.Scale.X / 2)))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool PositionRangesOverlap(float aStart, float aEnd, float bStart, float bEnd)
+        {
+            return aStart < bEnd && bStart < aEnd;
         }
     }
 }
